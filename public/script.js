8b25cd0db635fbb8977996d0e7aaf8423dd4493c
Кристиан Костадинов <133400423+Briefwebco@users.noreply.github.com>
2024-06-09 17:11:20 +0300

    Delete public directory

--- public/script.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-    let nav = document.getElementById("navbar");
-    if (nav.className === "navbar") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "navbar";
-    }
-}
